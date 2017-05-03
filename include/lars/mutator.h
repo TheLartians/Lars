e3283421831e5937e148b2a4b40675a8ebd96d97
Lars Melchior <thelartians@gmail.com>
2017-05-03 18:31:49 +0200

    make getter const

--- include/lars/mutator.h
+++ include/lars/mutator.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define MUTATOR_MEMBER(TYPE,NAME,DEFAULT,CODE) private: TYPE _ ## NAME DEFAULT; public: const TYPE &NAME(){ return _ ## NAME; } void set_ ## NAME(const TYPE &NAME){ _ ## NAME = NAME; CODE; }
+#define MUTATOR_MEMBER(TYPE,NAME,DEFAULT,CODE) private: TYPE _ ## NAME DEFAULT; public: const TYPE &NAME()const{ return _ ## NAME; } void set_ ## NAME(const TYPE &NAME){ _ ## NAME = NAME; CODE; }
 
