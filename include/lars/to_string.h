1b54241f3acbed416013cfef27bb6b662d8f40e5
Lars Melchior <thelartians@gmail.com>
2017-06-02 16:46:13 +0200

    added precision to to_string

--- include/lars/to_string.h
+++ include/lars/to_string.h
@@ -27,17 +27,31 @@
 
 namespace lars {
   
-  template <class T> std::string to_string(const T &obj){
+  template <class T> std::string to_string(const T &obj,int precision = 0){
     std::stringstream stream;
+    if(precision) stream.precision(precision);
     stream << obj;
     return stream.str();
   }
 
-  template <typename T> std::wstring to_wstring(T value ) {
-      std::ostringstream os ;
-      os << value ;
-      auto str = os.str(); 
-      return std::wstring(str.begin(),str.end()) ;
-    }
+  template <typename T> std::wstring to_wstring(T value,int precision = 0 ) {
+    std::wostringstream stream ;
+    if(precision) stream.precision(precision);
+    stream << value ;
+    return stream.str() ;
+  }
+  
+  template <typename T> T from_wstring(const std::wstring &string){
+    T value;
+    std::wistringstream(string) >> std::boolalpha >> value;
+    return value;
+  }
+  
+  template <typename T> T from_string(const std::string &string){
+    T value;
+    std::istringstream(string) >> std::boolalpha >> value;
+    return value;
+  }
+  
   
 }
