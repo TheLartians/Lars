50619bc653b5006278bf8b998ccd152f08fe9648
Lars Melchior <thelartians@gmail.com>
2017-06-04 20:45:28 +0200

    add mutator visibiliy arguments

--- include/lars/mutator.h
+++ include/lars/mutator.h
@@ -2,3 +2,5 @@
 
 #define MUTATOR_MEMBER(TYPE,NAME,DEFAULT,CODE) private: TYPE _ ## NAME DEFAULT; public: TYPE const &NAME()const{ return _ ## NAME; } void set_ ## NAME(TYPE const &NAME){ _ ## NAME = NAME; CODE; }
 
+#define MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY void set_ ## NAME(TYPE const &NAME){ _ ## NAME = NAME; CODE; }
+
