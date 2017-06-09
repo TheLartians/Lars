3e2679f6ccd69e11231d05666b67a2cef066bb92
Lars Melchior <thelartians@gmail.com>
2017-06-09 13:22:07 +0200

    update mutator setter method

--- include/lars/mutator.h
+++ include/lars/mutator.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MUTATOR_MEMBER(TYPE,NAME,DEFAULT,CODE) private: TYPE _ ## NAME DEFAULT; public: TYPE const &NAME()const{ return _ ## NAME; } void set_ ## NAME(TYPE const &NAME){ _ ## NAME = NAME; CODE; }
+#define MUTATOR_MEMBER(TYPE,NAME,DEFAULT,CODE) private: TYPE _ ## NAME DEFAULT; public: TYPE const &NAME()const{ return _ ## NAME; } void set_ ## NAME(TYPE const & __ ## NAME){ _ ## NAME = __ ## NAME; CODE; }
 
-#define MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY void set_ ## NAME(TYPE const &NAME){ _ ## NAME = NAME; CODE; }
+#define MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY void set_ ## NAME(TYPE const &__ ## NAME){ _ ## NAME = __ ## NAME; CODE; }
 
