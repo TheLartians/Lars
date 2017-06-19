708cec74bc91753fffc17bce74c3770e9c8a49e4
Lars Melchior <thelartians@gmail.com>
2017-06-20 00:07:18 +0200

    added float compare functions

--- include/lars/math.h
+++ include/lars/math.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <limits>
+
 namespace lars {
   
   template<class T> inline constexpr T pow(const T base, const unsigned exponent) {
@@ -8,6 +10,22 @@ namespace lars {
 
   template <typename T> static int sign(T val) {
     return (T(0) < val) - (val < T(0));
+  }
+  
+  template <typename T> bool are_approximately_equal(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()){
+    return std::abs(a - b) <= ( (std::abs(a) < std::abs(b) ? std::abs(b) : std::abs(a)) * epsilon);
+  }
+  
+  template <typename T> bool are_essentially_equal(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()){
+    return fabs(a - b) <= ( (std::abs(a) > std::abs(b) ? std::abs(b) : std::abs(a)) * epsilon);
+  }
+  
+  template <typename T> bool definitely_greater_than(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()){
+    return (a - b) > ( (std::abs(a) < std::abs(b) ? std::abs(b) : std::abs(a)) * epsilon);
+  }
+  
+  template <typename T> bool definitely_less_than(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()){
+    return (b - a) > ( (std::abs(a) < std::abs(b) ? std::abs(b) : std::abs(a)) * epsilon);
   }
   
 
