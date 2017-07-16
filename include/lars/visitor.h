685a562d9a4ccf0ff81e9d2a58921fd6233b4818
Lars Melchior <thelartians@gmail.com>
2017-07-16 15:21:49 +0200

    added ConstVisitor

--- include/lars/visitor.h
+++ include/lars/visitor.h
@@ -7,15 +7,18 @@
 #include <typeindex>
 #endif
 
+#ifndef LARS_VISITOR_NO_EXCEPTIONS
 #include <exception>
+#endif
 
 namespace lars{
   
   class VisitableBase;
   template <typename ... Args> class Visitor;
+  template <typename ... Args> class ConstVisitor;
   template <typename ... Args> class Visitable;
 
-  class VisitorBase{
+  template <template <typename ... Args> class Visitor> class VisitorBasePrototype{
 #ifdef LARS_VISITOR_NO_DYNAMIC_CAST
   protected:
     std::unordered_map<std::type_index,void *> derived_types;
@@ -31,12 +34,16 @@ namespace lars{
       return dynamic_cast<Visitor<T> *>(this);
     }
 #endif
-    virtual ~VisitorBase(){}
+    virtual ~VisitorBasePrototype(){}
   };
-
+  
+  using VisitorBase = VisitorBasePrototype<Visitor>;
+  using ConstVisitorBase = VisitorBasePrototype<ConstVisitor>;;
+  
   class VisitableBase{
   public:
     virtual void accept(VisitorBase &visitor) = 0;
+    virtual void accept(ConstVisitorBase &visitor)const = 0;
     virtual ~VisitableBase(){}
   };
   
@@ -54,26 +61,60 @@ namespace lars{
     virtual void visit(T &) = 0;
   };
   
-  template <> class Visitor<>:public VisitableBase{
-  protected:
-    static void visit(){}
+  template <> class Visitor<>{ };
+  
+  
+  template <typename First,typename Second,typename ... Rest> class ConstVisitor<First,Second,Rest...>:public ConstVisitor<First>,public ConstVisitor<Second,Rest...>{
+  public:
   };
   
+  template <typename T> class ConstVisitor<T>:public virtual ConstVisitorBase{
+  public:
+#ifdef LARS_VISITOR_NO_DYNAMIC_CAST
+    ConstVisitor(){
+      ConstVisitorBase::derived_types.emplace(typeid(ConstVisitor<T>),reinterpret_cast<void*>(this));
+    }
+#endif
+    virtual void visit(const T &) = 0;
+  };
+  
+  template <> class ConstVisitor<>{ };
+
+  
+#ifndef LARS_VISITOR_NO_EXCEPTIONS
   struct IncompatibleVisitorException:public std::exception{};
+#endif
   
   template <class T> class Visitable<T>:public virtual VisitableBase{
   public:
     
+#ifndef LARS_VISITOR_NO_EXCEPTIONS
     struct IncompatibleVisitorException:public lars::IncompatibleVisitorException{};
+#endif
     
     void accept(VisitorBase &visitor)override{
       if(auto casted = visitor.as_visitor_for<T>()){
         casted->visit(static_cast<T &>(*this));
       }
       else{
+#ifndef LARS_VISITOR_NO_EXCEPTIONS
         throw IncompatibleVisitorException();
+#endif
       }
     }
+    
+    void accept(ConstVisitorBase &visitor)const override{
+      if(auto casted = visitor.as_visitor_for<T>()){
+        casted->visit(static_cast<const T &>(*this));
+      }
+      else{
+#ifndef LARS_VISITOR_NO_EXCEPTIONS
+        throw IncompatibleVisitorException();
+#endif
+      }
+    }
+
+    
   };
   
   template <class T,class ... Bases> class Visitable<T,Bases...>:public virtual Bases ...{
@@ -84,24 +125,36 @@ namespace lars{
     }
     
     template <class Current,class Second,typename ... Rest> void try_to_accept(VisitorBase &visitor){
-#ifdef LARS_VISITOR_NO_DYNAMIC_CAST
       if(auto casted = visitor.as_visitor_for<T>()){
         casted->visit(static_cast<T &>(*this));
         return;
       }
-#else
-      if(auto * casted = dynamic_cast<Visitor<Current>*>(&visitor)){
-        casted->visit(static_cast<Current &>(*this));
-        return;
-      }
-#endif
       try_to_accept<Second,Rest ...>(visitor);
     }
+
+    template <class Current> void try_to_accept(ConstVisitorBase &visitor)const{
+      Current::accept(visitor);
+    }
+    
+    template <class Current,class Second,typename ... Rest> void try_to_accept(ConstVisitorBase &visitor)const{
+      if(auto casted = visitor.as_visitor_for<T>()){
+        casted->visit(static_cast<const T &>(*this));
+        return;
+      }
+      try_to_accept<Second,Rest ...>(visitor);
+    }
+
     
   public:
+    
     void accept(VisitorBase &visitor)override{
       try_to_accept<T, Bases...>(visitor);
     }
+    
+    void accept(ConstVisitorBase &visitor)const override{
+      try_to_accept<T, Bases...>(visitor);
+    }
+    
   };
 
   
