c185ae8516cf26b2b8efa8d8012f3cc5aaf75f74
Lars Melchior <TheLartians@users.noreply.github.com>
2017-08-01 22:40:32 +0200

    update

--- include/lars/mutator.h
+++ include/lars/mutator.h
@@ -1,6 +1,14 @@
 #pragma once
 
-#define MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY void set_ ## NAME(TYPE const &__ ## NAME){ _ ## NAME = __ ## NAME; CODE; }
+namespace lars{
+  namespace mutator_helper{
+    template <class T,typename ... Args> T initialize(const Args & ... args){
+      return T(args...);
+    }
+  }
+}
+  
+#define MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY template <typename ... Args> auto &set_ ## NAME(const Args & ... args){ _ ## NAME = ::lars::mutator_helper::initialize<TYPE>(args...); CODE; return *this; }
 
 
 #define MUTATOR_MEMBER_PRIVATE(TYPE,NAME,DEFAULT,CODE) MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,public:,private:);
