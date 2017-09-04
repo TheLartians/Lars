222625dce5256e89dedadaccb9566abff42c410e
Lars Melchior <TheLartians@users.noreply.github.com>
2017-09-04 20:19:28 +0200

    update

--- include/lars/type_index.h
+++ include/lars/type_index.h
@@ -1,17 +1,19 @@
 #pragma once
 
 namespace lars{
+  
+  using TypeIndex = unsigned;
 
-  template <class Index = unsigned> struct TypeIndexContext{
-    static Index type_count;
-    template <class T> struct TypeIndex{ static Index value; };
+  template <int CONTEXT = 0> struct TypeIndexContext{
+    static lars::TypeIndex type_count;
+    template <class T> struct Index{ static lars::TypeIndex value; };
   };
   
-  template <class Index> Index TypeIndexContext<Index>::type_count = 0;
-  template <class Index> template <class T> Index TypeIndexContext<Index>::TypeIndex<T>::value = TypeIndexContext<Index>::type_count++;
+  template <int CONTEXT> TypeIndex TypeIndexContext<CONTEXT>::type_count = 0;
+  template <int CONTEXT> template <class T> TypeIndex TypeIndexContext<CONTEXT>::Index<T>::value = TypeIndexContext<CONTEXT>::type_count++;
   
-  template <class T> unsigned get_type_index(){
-    return TypeIndexContext<>::TypeIndex<T>::value;
+  template <class T> TypeIndex get_type_index(){
+    return TypeIndexContext<>::Index<T>::value;
   }
 
 }
