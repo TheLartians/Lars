5f5a7a1ae991cb892a528e1220e97aed9c3ad551
Lars Melchior <TheLartians@users.noreply.github.com>
2017-11-20 17:54:23 +0100

    change approximetly equal to essentially equal and remove commparisons

--- include/lars/math.h
+++ include/lars/math.h
@@ -8,6 +8,14 @@ namespace lars {
     return v < 0 ? -v : v;
   }
   
+  template <typename T> inline T max(const T& a,const T& b) {
+    return a < b ? b : a;
+  }
+  
+  template <typename T> inline T min(const T& a,const T& b) {
+    return a < b ? a : b;
+  }
+  
   template<class T> inline constexpr T pow(const T base, const unsigned exponent) {
     return (exponent == 0) ? 1 : (exponent % 2 == 0) ? pow(base, exponent/2)*pow(base, exponent/2) : base * pow(base, (exponent-1)/2) * pow(base, (exponent-1)/2);
   }
@@ -21,20 +29,7 @@ namespace lars {
   }
   
   template <typename T> bool are_approximately_equal(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()){
-    return lars::abs(a - b) <= ( (lars::abs(a) < lars::abs(b) ? lars::abs(b) : lars::abs(a)) * epsilon);
+    return lars::abs(a - b) <= ( lars::min(lars::abs(a),lars::abs(b)) * epsilon);
   }
-  
-  template <typename T> bool are_essentially_equal(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()){
-    return fabs(a - b) <= ( (lars::abs(a) > lars::abs(b) ? lars::abs(b) : lars::abs(a)) * epsilon);
-  }
-  
-  template <typename T> bool definitely_greater_than(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()){
-    return (a - b) > ( (lars::abs(a) < lars::abs(b) ? lars::abs(b) : lars::abs(a)) * epsilon);
-  }
-  
-  template <typename T> bool definitely_less_than(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()){
-    return (b - a) > ( (lars::abs(a) < lars::abs(b) ? lars::abs(b) : lars::abs(a)) * epsilon);
-  }
-  
 
 }
