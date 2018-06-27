24b6fa8df30a25646df1a29883c5c43aa800b229
Lars Melchior <TheLartians@users.noreply.github.com>
2018-06-27 23:23:35 +0200

    update

--- include/lars/destructor.h
+++ include/lars/destructor.h
@@ -4,11 +4,17 @@
 
 namespace lars {
   
-  template <class F> struct Destructor{
+  struct DestructorBase{
+    virtual ~DestructorBase(){}
+  };
+  
+  template <class F> struct Destructor:public DestructorBase{
     F callback;
     Destructor(const F & f):callback(f){}
     ~Destructor(){ callback(); }
   };
+  
+  using SharedDestructor = std::shared_ptr<DestructorBase>;
   
   template <class F> std::shared_ptr<Destructor<F>> make_shared_destructor(const F &f){
     return std::make_shared<Destructor<F>>(f);
