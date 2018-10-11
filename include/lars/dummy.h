fc0265e16f9393bbe3dc64f3732ab13850d49748
Lars Melchior <TheLartians@users.noreply.github.com>
2018-10-11 10:25:57 +0200

    update

--- include/lars/dummy.h
+++ include/lars/dummy.h
@@ -2,8 +2,8 @@
 
 namespace lars {
   
+  class DummyClass{};
   template <typename D,typename T = void> struct DummyTemplate{ const static bool value = true; using type = T; };
-  
   template <typename ... Args> void dummy_function(const Args & ... args){ }
   
 }
