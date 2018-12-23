993abf3aec95abd49b48594d66a69b665bfdcd5a
Lars Melchior <TheLartians@users.noreply.github.com>
2018-12-23 10:29:04 +0100

    bugfix

---
+++ examples/any.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <lars/any.h>
+
+using namespace lars;
+
+int main(){
+  Any a = make_any<std::shared_ptr<std::string>>(std::make_shared<std::string>("test"));
+  std::cout << *a.get<std::shared_ptr<std::string>>() << std::endl;
+  std::cout << a.get<std::string>() << std::endl;
+  
+  Any b = make_any<float>(42.42);
+  std::cout << b.get<int>() << std::endl;
+  std::cout << b.get<double>() << std::endl;
+  
+  struct Base:Visitable<Base>{int a;};
+  struct Derived:public DVisitable<Derived, Base>{Derived(int v){ b=v; a=v+1; } int b;};
+
+  Any c = make_any<Derived>(42);
+  std::cout << c.get<Base>().a << std::endl;
+  std::cout << c.get<std::shared_ptr<Base>>()->a << std::endl;
+  std::cout << c.get<std::shared_ptr<Derived>>()->b << std::endl;
+  std::cout << c.get<Derived>().b << std::endl;
+
+  Any d = make_any<std::shared_ptr<Derived>>(std::make_shared<Derived>(45));
+  std::cout << d.get<Base>().a << std::endl;
+  std::cout << d.get<std::shared_ptr<Base>>()->a << std::endl;
+  std::cout << d.get<std::shared_ptr<Derived>>()->b << std::endl;
+  std::cout << d.get<Derived>().b << std::endl;
+}
